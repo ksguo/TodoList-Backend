409ea926a2d93bc27adee232856e3cf41ec7dc74
ks_guo <kguo1@smail.uni-koeln.de>
2025-02-22 23:37:20 +0100

    add db setup

--- app/schema/user.py
+++ app/schema/user.py
@@ -12,5 +12,69 @@ class UserBase(SQLModel):
 
 
 # Properties to receive via API on creation
+
+
+# for internal use
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=40)
+
+
+# for external use
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+
+
+# Properties to receive via API on update
+
+
+# admin use
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    password: str | None = Field(default=None, min_length=8, max_length=40)
+
+
+# user use
+class UserUpdateMe(SQLModel):
+    email: EmailStr | None = Field(default=None, max_length=255)
+
+
+# user use
+class UpdatePassword(SQLModel):
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
+
+
+# Database model, database table inferred from class name
+# Database storage and ORM operations
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
+
+
+# Properties to return via API, id is always required
+# Returns public information of a single user
+class UserPublic(UserBase):
+    id: uuid.UUID
+
+
+# Return paged/batch user query results
+class UsersPublic(SQLModel):
+    data: list[UserPublic]
+    count: int
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
